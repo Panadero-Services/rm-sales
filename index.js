cb4f5f4a9c3371f9e6411d37cb2e3e692af1cec0
lieuwebakker <3v8z4q@gmail.com>
2025-03-12 10:22:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 
-import { moduleName, moduleGit, moduleVersion, moduleDate, moduleAuthor, moduleTitle, colors, Order, OrderLine, orderStatus, orderLineStatus, applyDiscountLine, applyDiscount };
+import { moduleName, moduleGit, moduleVersion, moduleDate, moduleAuthor, moduleTitle, colors, Order, OrderLine, orderStatus, orderLineStatus, applyDiscountLine, applyDiscount }
 
-export { moduleName, moduleGit, moduleVersion, moduleDate, moduleAuthor, moduleTitle, colors, Order, OrderLine, orderStatus, orderLineStatus, applyDiscountLine, applyDiscount };
+export { moduleName, moduleGit, moduleVersion, moduleDate, moduleAuthor, moduleTitle, colors, Order, OrderLine, orderStatus, orderLineStatus, applyDiscountLine, applyDiscount }
